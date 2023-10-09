fead637a88d0f2bab553eac68f4da846de7b9f07
Shaude123 <111890801+Shaude123@users.noreply.github.com>
2023-10-09 15:36:56 -0400

    Add files via upload

---
+++ Descriptive_Variable_Fuctions.R
@@ -0,0 +1,34 @@
+variable1 <-c(1,2,3,4,5,6,7,8,9,10,1900)
+
+#Caculate mean for variable1
+
+mean(variable1)
+
+#Calculate median for variable1
+
+median(variable1)
+
+#Calculate standard deviation for variable1
+
+sd(variable1)
+
+#Calculate Mode for (variable1)
+
+#Install following libray
+
+install.packages('DescTools'))
+
+#Load the library
+
+library(DescTools)
+
+Mode(variable1)
+
+#Calculate range/Min and Max value
+
+min(variable1)
+max(variable1)
+
+#Calculate variance in R
+
+var(variable1)
